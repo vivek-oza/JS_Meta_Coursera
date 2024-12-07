7c239d76c1faf2417ac40b0f5bebc53d1430e437
vivek-oza <112532672+vivek-oza@users.noreply.github.com>
2024-12-07 12:46:12 +0530

    No changes

--- MODULE_1/03_conversionOperations.js
+++ MODULE_1/03_conversionOperations.js
@@ -12,31 +12,31 @@ let score6 = undefined;
 
 // 
 
-// console.log(typeof(score));
+console.log(typeof(score));
 
-// let valueInNumber = Number(score);  //DATA TYPE CONVERSION TO NUMBER
-// console.log(typeof(valueInNumber)); //number
-// console.log(valueInNumber);  //33
+let valueInNumber = Number(score);  //DATA TYPE CONVERSION TO NUMBER
+console.log(typeof(valueInNumber)); //number
+console.log(valueInNumber);  //33
 
-// let valueInNumber2 = Number(score2);  //DATA TYPE CONVERSION TO NUMBER
-// console.log(typeof(valueInNumber2));
-// console.log(valueInNumber2);    //number , NaN
+let valueInNumber2 = Number(score2);  //DATA TYPE CONVERSION TO NUMBER
+console.log(typeof(valueInNumber2));
+console.log(valueInNumber2);    //number , NaN
 
-// let valueInNumber3 = Number(score3);  //DATA TYPE CONVERSION TO NUMBER
-// console.log(typeof(valueInNumber3));
-// console.log(valueInNumber3);    //number , NaN
+let valueInNumber3 = Number(score3);  //DATA TYPE CONVERSION TO NUMBER
+console.log(typeof(valueInNumber3));
+console.log(valueInNumber3);    //number , NaN
 
-// let valueInNumber4 = Number(score4);  //DATA TYPE CONVERSION TO NUMBER
-// console.log(typeof(valueInNumber4));
-// console.log(valueInNumber4);      //number , 1
+let valueInNumber4 = Number(score4);  //DATA TYPE CONVERSION TO NUMBER
+console.log(typeof(valueInNumber4));
+console.log(valueInNumber4);      //number , 1
 
-// let valueInNumber5 = Number(score5);  //DATA TYPE CONVERSION TO NUMBER
-// console.log(typeof(valueInNumber5));
-// console.log(valueInNumber5);      //number , 0
+let valueInNumber5 = Number(score5);  //DATA TYPE CONVERSION TO NUMBER
+console.log(typeof(valueInNumber5));
+console.log(valueInNumber5);      //number , 0
 
-// let valueInNumber6 = Number(score6);  //DATA TYPE CONVERSION TO NUMBER
-// console.log(typeof(valueInNumber6));
-// console.log(valueInNumber6);    //number , NaN
+let valueInNumber6 = Number(score6);  //DATA TYPE CONVERSION TO NUMBER
+console.log(typeof(valueInNumber6));
+console.log(valueInNumber6);    //number , NaN
 
 // // NOTES
 // "33" -> 33
