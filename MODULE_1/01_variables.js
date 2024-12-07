ad7417633b1c9c06ee029f9e3d004b4681f8b62b
vivek-oza <112532672+vivek-oza@users.noreply.github.com>
2024-12-07 11:09:21 +0530

    Data types

---
+++ MODULE_1/01_variables.js
@@ -0,0 +1,17 @@
+/*
+Variables.
+var
+let
+const
+*/
+
+var favFood;        //Variable declaration
+favFood = "Pizza"   //Initialization
+
+console.log("I love",favFood)
+
+
+function someFunc  ()  {
+    let betterVarWithoutScopeIssue = "Yeah nerd, I got it"
+}
+
---
+++ MODULE_1/02_datatypes.js
@@ -0,0 +1,44 @@
+// DATA TYPES
+
+/*
+
+number = int, float, bigint etc...
+
+string -> " " or ' ' (" " preferred)
+
+boolean = true or false
+
+null = absence of value.
+
+undefined = value not assigned yet.
+
+symbol
+
+object
+*/
+
+
+// There is newer version of JS
+// we use -> "use strict" //Treat all JS code as Newer version
+
+// alert(2+3)  //WE AR=E USING NODE JS AND NOT BROWSER
+
+// Where to use semicolon -> we can use it, or not, its actually a redablity choice
+
+console.log('pizza');
+
+let age = 18
+let name = "Vivek" 
+let isLoggedIn = false 
+
+let state; //UNDEFINED VARIABLE, VALUE NOT ASSIGNED YET
+let state2 = null; //EMPTY , THE EMPTY VALUE IS GIVEN
+
+//symbol -> for uiqueness
+
+// object
+
+console.log(typeof(state));
+console.log(typeof("pizza"));
+console.log(typeof(undefined)); //undefined
+console.log(typeof(null)); //object
