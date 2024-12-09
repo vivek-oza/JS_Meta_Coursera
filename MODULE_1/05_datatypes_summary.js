fe41e5a981d4b8b7857bbb9f3695e34a8bb27fa1
vivek-oza <112532672+vivek-oza@users.noreply.github.com>
2024-12-09 13:03:04 +0530

    Memory Types, STACK and HEAP, in MODULE_1,  05

--- MODULE_1/05_datatypes_summary.js
+++ MODULE_1/05_datatypes_summary.js
@@ -43,72 +43,124 @@ Changes to the copy do not affect the original value.
 
 
 
-//Primitive datatypes 7 categories
+// //Primitive datatypes 7 categories
 
-// String
-// Number
-// Boolean
-// null
-// undefined
-// Symbol
-// BigInt
-
-
-const score = 100;
-const scoreValue = 100.3;
-
-const isLoggedIn = false;
-
-const outsideTemp = null;
-
-let username;
-//or let username = undefined;
-
-//Symbol
-
-const id = Symbol('123');
-const anotherId = Symbol('123');
-
-console.log(id === anotherId);      //false 
+// // String
+// // Number
+// // Boolean
+// // null
+// // undefined
+// // Symbol
+// // BigInt
 
 
+// const score = 100;
+// const scoreValue = 100.3;
 
-// BigInt
+// const isLoggedIn = false;
 
-const bigNumber = 99999999999999999999n;    //bigint
+// const outsideTemp = null;
 
-console.log(bigNumber);
-console.log(typeof(bigNumber));
+// let username;
+// //or let username = undefined;
+
+// //Symbol
+
+// const id = Symbol('123');
+// const anotherId = Symbol('123');
+
+// console.log(id === anotherId);      //false 
 
 
 
-// NON Primitive (   REFERENCE TYPES   )
+// // BigInt
 
-// Array
-// Object
-// Functions
+// const bigNumber = 99999999999999999999n;    //bigint
 
-
-// ARRAY -> square brackets means array
-const heros = ["shaktiman", "doga", "naagraj"];
-
-const myObj = {
-    name: "vivek",
-    age: 20
-};
+// console.log(bigNumber);
+// console.log(typeof(bigNumber));
 
 
-const myFunction = function(){
-    console.log("Hello i love pixxa");
-}
 
-myFunction()
+// // NON Primitive (   REFERENCE TYPES   )
+
+// // Array
+// // Object
+// // Functions
 
 
-console.log(typeof(heros));     //object
-console.log(typeof(myObj));     //object
-console.log(typeof(myFunction));    //function (object function) 
+// // ARRAY -> square brackets means array
+// const heros = ["shaktiman", "doga", "naagraj"];
+
+// const myObj = {
+//     name: "vivek",
+//     age: 20
+// };
+
+
+// const myFunction = function(){
+//     console.log("Hello i love pixxa");
+// }
+
+// myFunction()
+
+
+// console.log(typeof(heros));     //object
+// console.log(typeof(myObj));     //object
+// console.log(typeof(myFunction));    //function (object function) 
 
 
 // const testVar = null;
 // console.log(typeof testVar);    //null variable's dt is object
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+/* 
+
+    // NOTES
+    
+    MEMORY TYPES IN JAVA SCRIPT
+    STACK   AND   HEAP
+    
+    Stack ->    USED IN Primitive data types
+
+    Heap -> USED IN Non-Primitive data types
+
+
+    PLEASE WATCH CHAI CODE -> VIDEO 10 -> IN CHAI AUR JAVASCRIPT
+
+ */
+
+let myYTName = "CodingsCool";
+
+let anotherName = myYTName;
+anotherName = "Bhai Codez"
+
+console.log(myYTName);
+console.log(anotherName);   //when change happened in the anotherName, it doesnt affect the myYTNAME because, when we assigned myYTNAME to anotherName, only a copy was given to it, and not the original value's reference, (that would happen in non-primitive data type)
+
+
+
+
+
+
+let user1 = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
+};
+
+let user2 = user1;
+
+console.log(user1);
+
+console.log(user2);
+
+user2.email = "vivek@google.com"
+
+console.log(user1);
+console.log(user2);     // WE CHANGED DATA IN USER 2, BUT IT ALSO CHANGED IN, THE ORIGINAL , USER 1 , BECAUSE , IT WAS IN HEAP MEMORY, BECAUSE IT WAS A NON PRIMITIVE DATATYPE(AN OBJECT)
+
+
+
