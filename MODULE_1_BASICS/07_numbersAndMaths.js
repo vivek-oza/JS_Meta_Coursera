0357666db9e2f028da405ece05200f268ad46297
vivek-oza <112532672+vivek-oza@users.noreply.github.com>
2024-12-31 13:53:17 +0530

    Clean up

--- MODULE_1_BASICS/07_numbersAndMaths.js
+++ MODULE_1_BASICS/07_numbersAndMaths.js
@@ -23,5 +23,36 @@
 // console.log(Number.MIN_VALUE); //5e-324
 
 
-//#### MATHS ####
+// //#### MATHS ####
+
+// console.log(Math);
+
+// console.log(Math.abs(-10));     //10
+// console.log(Math.round(4.3));
+// console.log(Math.round(4.6));
+// console.log(Math.floor(4.7));
+// console.log(Math.ceil(4.1));
+
+// console.log(Math.sqrt(4));
+
+// console.log(Math.min(2,4,0,5,69,23,2));
+// console.log(Math.max(2,4,0,5,69,23,2));
+
+// // MORE METHODS, JUST CONSOLE LOG "Math" IN CONSOLE BROWSER
+
+
+// SIMPLE DICE
+
+console.log(Math.random());
+
+console.log((Math.random()*10) + 1);
+
+console.log(Math.floor(Math.random() * 10) + 1); // random from 1 to 10
+
+
+//RANDOM FROM MIN TO MAX
+
+min = 1
+max = 6
+console.log( Math.floor(Math.random() * (max - min + 1)) + min);
 
---
+++ MODULE_2_BASIC_DS/09_arrays.js
@@ -0,0 +1,13 @@
+// ARRAY
+// Array is Collection of items in JS, Can store multiple datatypes elements in Single variable (same array)
+// ARRAY IN JS ARE RESIZABLE
+
+
+const arrayExample= [0,5,2,1,3,true,"Vivek"];
+
+const mrArray = [0,5,2,1,3];
+
+console.log("hey");
+
+console.log(mrArray[3]);
+
