f81bdff67e568612dc61cc24e03a72bfcb746df7
vivek-oza <112532672+vivek-oza@users.noreply.github.com>
2024-12-31 11:13:46 +0530

    Clean up

---
+++ MODULE_1/07_numbersAndMaths.js
@@ -0,0 +1,27 @@
+// // NUMBER AND MATHS JAVA SCRIPT
+
+// //      NUMBERS     //
+
+// const hitpoints = 100;
+// console.log(hitpoints);     //100
+
+// const bankBal = new Number(100000);
+// console.log(bankBal);       //[Number: 100000]
+
+// console.log(bankBal.toString());
+// console.log(bankBal.toString().length);     // Now we can use string methods on this number
+
+
+// console.log(bankBal.toFixed(2));
+
+// const otherNum = 21.9438;
+// console.log(otherNum.toPrecision(2));    //22
+
+// const testNumber= 1000000;
+// console.log(testNumber.toLocaleString('en-IN'));     //10,00,000
+// console.log(Number.MAX_VALUE); //1.7976931348623157e+308
+// console.log(Number.MIN_VALUE); //5e-324
+
+
+//#### MATHS ####
+
